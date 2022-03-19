eb78589d7c5e1a33e8d4f3d42200f2dc0a6e9b0e
PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>
2022-03-19 11:26:05 +0100

    add setup.py

---
+++ readme.md
@@ -0,0 +1,3 @@
+![GitHub repo size](https://img.shields.io/github/repo-size/PaddeCraft/Log4py?label=total%20size) ![Lines of code](https://img.shields.io/tokei/lines/github/PaddeCraft/Log4py) ![GitHub issues](https://img.shields.io/github/issues-raw/PaddeCraft/Log4py?label=issues)
+
+## Log4py
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Log4py',
+    version='0.0.1',
+    description='A simple python logging package',
+    url='https://github.com/PaddeCraft/Log4py',
+    author='PaddeCraft',
+    license='BSD 2-clause',
+    packages=['Log4py'],
+    install_requires=['rich'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+)
